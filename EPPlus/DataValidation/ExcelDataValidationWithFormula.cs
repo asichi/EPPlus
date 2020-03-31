0425f014c7bc8cd89d30ca4f13634513235e79d9
Anthony Sichi <asichi@swhd.org>
2020-03-30 18:38:37 -0700

    Fix Open Issue #481

--- EPPlus/DataValidation/ExcelDataValidationWithFormula.cs
+++ EPPlus/DataValidation/ExcelDataValidationWithFormula.cs
@@ -96,8 +96,10 @@ namespace OfficeOpenXml.DataValidation
 
         public override void Validate()
         {
-            base.Validate();
-            if (ValidationType != ExcelDataValidationType.List && (Operator == ExcelDataValidationOperator.between || Operator == ExcelDataValidationOperator.notBetween))
+             base.Validate();
+            if (ValidationType != ExcelDataValidationType.List 
+                && ValidationType != ExcelDataValidationType.Custom  
+                && (Operator == ExcelDataValidationOperator.between || Operator == ExcelDataValidationOperator.notBetween))
             {
                 if (string.IsNullOrEmpty(Formula2Internal))
                 {
